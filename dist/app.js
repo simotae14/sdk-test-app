e81b834650ca843e4770f23441d8c21958eef935
Simone Taeggi <startae14@gmail.com>
2023-04-04 17:55:52 +0200

    check all the SDK methods

--- dist/app.js
+++ dist/app.js
@@ -8,5 +8,19 @@ const client = new sdk_with_ts_2_1.default({
     apiKey: '123',
 });
 client.getPosts().then((p) => {
+    console.log('GETPOSTS results');
+    console.log(p);
+});
+client.getPostById(1).then((p) => {
+    console.log('GETPOSTBYID result');
+    console.log(p);
+});
+client.createPost({
+    title: "foo",
+    body: "bar",
+    userId: 1
+}).then((p) => {
+    console.log('CREATEPOST result');
+    console.log(`New post is created with ${p.id}`);
     console.log(p);
 });
--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,21 @@ const client = new Typicode({
 });
 
 client.getPosts().then((p) => {
+  console.log('GETPOSTS results');
+  console.log(p);
+});
+
+client.getPostById(1).then((p) => {
+  console.log('GETPOSTBYID result');
+  console.log(p);
+});
+
+client.createPost({
+  title: "foo",
+  body: "bar",
+  userId: 1
+}).then((p) => {
+  console.log('CREATEPOST result');
+  console.log(`New post is created with ${p.id}`)
   console.log(p);
 });
